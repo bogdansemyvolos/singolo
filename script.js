1fbbc6e6bf2a608f78ee1e3d58c680d4485fa2af
bohdansemyvolos <b.semivolos@agilites.com>
2020-03-25 23:27:16 +0200

    fix: implement slidear according to new requirements

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const PORTFOLIO_PICS = document.querySelectorAll('.portfolio__pictures img');
 const SUBMIT_BUTTON = document.getElementById('submit');
 const CLOSE_MODAL_BUTTON = document.getElementById('close-btn');
 const FORM = document.getElementById('contact_form');
-var slideIndex = 1;
+const dots = document.querySelectorAll('.slider-dots_item');
 
 document.addEventListener('scroll', onScroll);
 
@@ -58,33 +58,6 @@ CLOSE_MODAL_BUTTON.addEventListener('click', () => {
   FORM.reset();
 });
 
-function showSlides(n) {
-  var slides = document.getElementsByClassName('slider__main')[0].querySelectorAll('div.first_slide, div.second_slide');
-  var dots = document.querySelectorAll('.slider-dots_item');
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  slides.forEach(el => el.classList.add('hidden'));
-  dots.forEach(el => el.classList.remove('active_dot'));
-  slides[slideIndex - 1].classList.remove('hidden');
-  dots[slideIndex - 1].classList.add('active_dot');
-}
-
-function plusSlide() {
-  showSlides(slideIndex += 1);
-}
-
-function minusSlide() {
-  showSlides(slideIndex -= 1);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function hideShowElementById(id) {
   var el = document.getElementById(id);
   if (el.classList.contains('hidden')) {
@@ -115,4 +88,89 @@ function onScroll(event) {
       });
     }
   });
+}
+
+//slider
+var
+  _mainElement = document.querySelector('.slider__main'),
+  _sliderWrapper = _mainElement.querySelector('.slider__wrapper'),
+  _sliderItems = _mainElement.querySelectorAll('div.first_slide, div.second_slide'),
+  _sliderControls = _mainElement.querySelectorAll('.control_prev_active, .control_next_active'),
+  _sliderControlLeft = _mainElement.querySelector('.control_prev_active'),
+  _sliderControlRight = _mainElement.querySelector('.control_next_active'),
+  _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width),
+  _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width),
+  _positionLeftItem = 0,
+  _transform = 0,
+  _step = _itemWidth / _wrapperWidth * 100,
+  _items = [];
+
+
+_sliderItems.forEach(function (item, index) {
+  _items.push({ item: item, position: index, transform: 0 });
+});
+
+_sliderControls.forEach(el => el.addEventListener('click', (event) => {
+  event.preventDefault();
+  var direction = event.target.classList.contains('control_next_active') || event.target.parentElement.classList.contains('control_next_active') ? 'right' : 'left';
+  transformItem(direction);
+}));
+
+var position = {
+  getItemMin: function () {
+    var indexItem = 0;
+    _items.forEach(function (item, index) {
+      if (item.position < _items[indexItem].position) {
+        indexItem = index;
+      }
+    });
+    return indexItem;
+  },
+  getItemMax: function () {
+    var indexItem = 0;
+    _items.forEach(function (item, index) {
+      if (item.position > _items[indexItem].position) {
+        indexItem = index;
+      }
+    });
+    return indexItem;
+  },
+  getMin: function () {
+    return _items[position.getItemMin()].position;
+  },
+  getMax: function () {
+    return _items[position.getItemMax()].position;
+  }
+}
+
+function transformItem(direction) {
+  var active_dot = Array.from(dots).map(el => el.classList.contains('active_dot')).indexOf(true);
+  dots.forEach(el => el.classList.remove('active_dot'));
+  var nextItem;
+  if (direction === 'right') {
+    _positionLeftItem++;
+    if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) > position.getMax()) {
+      nextItem = position.getItemMin();
+      _items[nextItem].position = position.getMax() + 1;
+      _items[nextItem].transform += _items.length * 100;
+      _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+    }
+    _transform -= _step;
+    active_dot = active_dot + 1 >= dots.length ? active_dot = 0 : active_dot + 1;
+    dots[active_dot].classList.add('active_dot');
+  }
+  if (direction === 'left') {
+    _positionLeftItem--;
+    if (_positionLeftItem < position.getMin()) {
+      nextItem = position.getItemMax();
+      _items[nextItem].position = position.getMin() - 1;
+      _items[nextItem].transform -= _items.length * 100;
+      _items[nextItem].item.style.transform = 'translateX(' + _items[nextItem].transform + '%)';
+    }
+    _transform += _step;
+    active_dot = active_dot - 1 < 0 ? dots.length - 1 : active_dot - 1;
+    dots[active_dot].classList.add('active_dot');
+  }
+  _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+
 }
